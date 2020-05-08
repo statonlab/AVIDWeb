7de7b38c8c65c4c8a7a049c6fd0adc149f777c25
jwest60 <jwest2178@gmail.com>
2020-05-08 15:26:21 -0400

    added plant controller testing

--- database/factories/PlantFactory.php
+++ database/factories/PlantFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(Plant::class, function (Faker $faker) {
     return [
-        //
+        'plot_id' => factory(\App\Plot::class)->create()->id,
+        'user_id' => factory(\App\User::class)->create()->id,
+        'plant_type_id' => \App\PlantType::first()->id,
+        'species_id' => factory(\App\Species::class)->create()->id,
+        'tag' => $faker->numberBetween(1000, 10000),
+        'quadrant' => 'Northeast',
+        'collected_at' => now(),
+        'stems' => $faker->numberBetween(10, 20),
+        'flowers' => $faker->numberBetween(10, 20),
     ];
 });
---
+++ tests/Feature/PlantControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Plant;
+use App\Plot;
+use App\PlantType;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PlantControllerTest extends TestCase
+{
+    use DatabaseTransactions, WithFaker;
+
+    public function testListingPlants()
+    {
+        $user = $this->makeAdmin();
+
+        $this->actingAs($user);
+
+        /** @var Plant $plant */
+        $plant = factory(Plant::class)->create();
+
+        $response = $this->get("/web/plots/$plant->plot_id/plants");
+        $response->assertSuccessful()->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'tag',
+                    'species_name',
+                    'species_id',
+                ]
+            ],
+        ]);
+    }
+
+    public function testRetrievingPlant()
+    {
+        $user = $this->makeAdmin();
+        $this->actingAs($user);
+
+        /** @var Plant $plant */
+        $plant = factory(Plant::class)->create();
+
+        $response = $this->get("/web/plants/$plant->id");
+
+        $response->assertSuccessful()->assertJsonStructure([
+            'id',
+            'tag',
+            'species_id',
+        ]);
+    }
+
+    public function testCreatingPlant()
+    {
+        $user = $this->makeAdmin();
+        $this->actingAs($user);
+
+        /** @var Plot $plot */
+        $plot = factory(Plot::class)->create();
+
+        $this->post("/web/plots/$plot->id/plants", [
+            'species_id' => factory(\App\Species::class)->create()->id,
+            'plant_type_id' => PlantType::first()->id,
+            'tag' => 1,
+            'quadrant' => 'Northeast',
+        ])->assertSuccessful();
+    }
+
+    public function testUpdatingPlant()
+    {
+        $user = $this->makeAdmin();
+        $this->actingAs($user);
+
+        /** @var Plant $plant */
+        $plant = factory(Plant::class)->create([
+            'tag' => 1,
+        ]);
+
+        $response = $this->put("/web/plants/$plant->id", [
+            'species_id' => $plant->species_id,
+            'plant_type_id' => $plant->plant_type_id,
+            'tag' => 2,
+            'quadrant' => 'Northeast',
+        ]);
+
+        $response->assertSuccessful()->assertJsonFragment([
+            'tag' => 2,
+        ]);
+    }
+
+    public function testDeletingPlant()
+    {
+        $user = $this->makeAdmin();
+        $this->actingAs($user);
+
+        /** @var Plant $plant */
+        $plant = factory(Plant::class)->create();
+
+        $response = $this->delete("/web/plants/$plant->id");
+
+        $response->assertSuccessful();
+    }
+}
