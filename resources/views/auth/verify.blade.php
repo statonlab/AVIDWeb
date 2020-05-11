ce2bfc64feddcefac4321372c760922d8df995fc
jwest60 <jwest2178@gmail.com>
2020-05-11 12:47:45 -0400

    added email verification

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -8,8 +8,11 @@
                     <div class="row align-items-stretch flex-lg-row flex-column-reverse">
                         <div class="col-lg-4 bg-primary rounded-left align-items-center d-flex zIndex-5 auth-left">
                             <div class="card-body text-white py-5">
-                                <p class="font-weight-bold text-center text">
+                                <p class="font-weight-bold text-center text mb-5">
                                     Welcome to AVID
+                                </p>
+                                <p class="font-weight-light text-center mb-5">
+                                    Before proceeding, please check your email for a verification link.
                                 </p>
                             </div>
                             <div class="auth-bg-eclipse">
@@ -27,7 +30,7 @@
                                     </div>
                                 @endif
 
-                                {{ __('Before proceeding, please check your email for a verification link.') }}
+                                {{ __('A verification link has been sent to your email.') }}
                                 {{ __('If you did not receive the email') }},
                                 <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                                     @csrf
@@ -37,19 +40,6 @@
                                 </form>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card border-0 shadow">
-                    <div class="card-header border-0">{{ __('Verify Your Email Address') }}</div>
-
-                    <div class="card-body">
-
                     </div>
                 </div>
             </div>
