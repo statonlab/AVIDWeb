6a2682ccd783300244de2f2349cddf8654247c1e
jwest60 <jwest2178@gmail.com>
2020-06-09 12:34:59 -0400

    added how to volunteer section to landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -116,15 +116,46 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h4 class="text-center" id="how-to-volunteer">How to Volunteer</h4>
-
-                    <p>
-                        Step 1
-                    </p>
-                    <p>Step 2</p>
-                    <p>
-                        Step 3
-                    </p>
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="text-center mb-4" id="how-to-volunteer">How to Volunteer</h4>
+                            <div class="mb-2">
+                                <strong>What You Will Do</strong>
+                            </div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <ul class="mb-4">
+                                        <li>Collect data in your woods or land in your community and enter it here to track tree, shrub and wildflower response to deer browsing over time.</li>
+                                        <li>Learn to identify important spring wildflower and tree species</li>
+                                        <li>Learn to recognize evidence of deer impacts based on the presence or absence of key wildflower, shrub, and tree indicator species</li>
+                                        <li>Contribute to a statewide database to document how deer impacts are changing through time. These data can help guide deer management decisions at local and state levels.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-4">
+                                    <img class="img-fluid mb-0" src="/img/about-deer.jpg" />
+                                    <p class="text-muted text-right">Credit: Haliburton Forest</p>
+                                </div>
+                            </div>
+                            <div class="mb-2">
+                                <strong>How To Get Started</strong>
+                            </div>
+                            <p class="mb-2">
+                                <b>Step 1:</b> <a href="/register">Register</a> and create an account. (This registration will allow you to enter and access your data online)
+                            </p>
+                            <p class="mb-1">
+                                <b>Step 2:</b> Learn about the methods you will use in one of the following ways:
+                            </p>
+                            <ul class="mb-2">
+                                <li>Visit the <a href="/resources/manual">Resources</a> tab at the top of the page and download the AVID manual. The manual can be printed and you can read it at your leisure or take it into the field with you. Data sheets are included at the end of the manual.</li>
+                                <li>Read through the <a href="/site-preparation">Site Preparation</a> and <a href="/user-guides/getting-started">User Guides</a> tabs here at the web site. All of the information in the manual is also present here. You can then print the data sheets you will need by clicking on the <a href="/resources/data-sheets">Resources</a> tab and downloading the data sheets.</li>
+                                <li>View <a href="https://www.youtube.com/watch?v=nsNXNos27IM">this recorded presentation</a> about using the AVID method, then download the data sheets through the <a href="/resources/data-sheets">Resources</a> tab.</li>
+                                <li>Attend an AVID training</li>
+                            </ul>
+                            <p class="mb-2">
+                                <b>Step 3:</b> Collect your data and enter it via the dashboard.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
