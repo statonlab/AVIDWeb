8f523288d3fe16594cb85cdca7757b999c3b60e3
jwest60 <jwest2178@gmail.com>
2020-06-15 12:45:17 -0400

    added logos for cornell and ut

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -19,10 +19,23 @@
                 </a>
             </div>
 
-            <div class="mb-1 text-white">
+            <div class="mb-4 text-white">
                 <a class="text-white" href="/resources/data-sheets">
                     Resources
                 </a>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-3">
+                    <a href="https://www.cornell.edu/" >
+                        <img class="img-fluid" src="/img/cornell_logo_white.svg" />
+                    </a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="https://www.utk.edu/">
+                        <img class="img-fluid" src="/img/ut3.png" />
+                    </a>
+                </div>
             </div>
 
             <div class="flex flex-wrap">
@@ -33,3 +46,11 @@
         </div>
     </div>
 </div>
+
+<style scoped>
+    .card-img-top {
+        object-fit: cover;
+        width: 100%;
+        height: 200px;
+    }
+</style>
