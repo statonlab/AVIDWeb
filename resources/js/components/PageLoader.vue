e46d478d1686e4cd2ffbe618876a03e83982354f
jwest60 <jwest2178@gmail.com>
2020-07-14 11:48:00 -0400

    implemented code splitting

---
+++ resources/js/components/PageLoader.vue
@@ -0,0 +1,17 @@
+<template>
+    <inline-spinner/>
+</template>
+
+<script>
+  import Modal from './Modal/Modal'
+  import ModalCard from './Modal/ModalCard'
+  import InlineSpinner from './InlineSpinner'
+  export default {
+    name: 'PageLoader',
+    components: {InlineSpinner, ModalCard, Modal},
+  }
+</script>
+
+<style scoped>
+
+</style>
