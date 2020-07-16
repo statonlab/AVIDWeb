e6c9c5758b83c07fb38f8c180ce1ef518dbf162d
jwest60 <jwest2178@gmail.com>
2020-07-16 13:01:17 -0400

    added screen when no events exist

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -4,12 +4,24 @@
     <div>
         @include('components.navbar')
         <div class="container py-4">
-            <div class="col-12">
-                <h1 class="mb-4">Events</h1>
-                <div class="row">
-                    @each('components.event_card', $events, 'event')
+            @if(! empty($events))
+                <div class="col-12">
+                    <h1 class="mb-4">Events</h1>
+                    <div class="row">
+                        @each('components.event_card', $events, 'event')
+                    </div>
                 </div>
-            </div>
+            @else
+                <div class="row">
+                    <div class="col-6 my-auto d-block">
+                        <p class="mb-0 text-muted text-center">There are no events at this time.</p>
+                        <p class="text-muted text-center">Please check again later.</p>
+                    </div>
+                    <div class="col-6">
+                        <img class="img-fluid mx-auto d-block" src="/img/no_events.svg" />
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 
