0ccacaa7f92f86ecf774cfbd1f14c177599d62a5
jwest60 <jwest2178@gmail.com>
2020-07-20 16:27:28 -0400

    updated listener

--- app/Listeners/MeasurementCreatedListener.php
+++ app/Listeners/MeasurementCreatedListener.php
@@ -20,34 +20,32 @@ class MeasurementCreatedListener implements ShouldQueue
     {
         $measurement = $event->measurement;
 
-        if ($measurement->plot->last_measured_at === null) {
-            $site = $measurement->site;
+        $site = $measurement->site;
 
-            if ($site->user->email_verified_at === null) {
-                return;
-            }
-
-            /** @var \App\Reminder */
-            $reminder = Reminder::create([
-                'days' => 0,
-                'user_id' => $site->user->id,
-                'site_id' => $site->id,
-            ]);
-
-            for ($i = now()->addYear(); $i < now()->addYears(10); $i->addYear()) {
-                ReminderEvent::create([
-                    'reminder_id' => $reminder->id,
-                    'site_id' => $site->id,
-                    'date' => $i->copy()->subWeek(),
-                ]);
-
-                ReminderEvent::create([
-                    'reminder_id' => $reminder->id,
-                    'site_id' => $site->id,
-                    'date' => $i->copy()->subMonth(),
-                ]);
-            }
+        if ($site->user->email_verified_at === null) {
+            return;
         }
+
+        /** @var \App\Reminder */
+        $reminder = Reminder::firstOrCreate([
+            'days' => 0,
+            'user_id' => $site->user->id,
+            'site_id' => $site->id,
+        ]);
+
+        $reminder->events()->delete();
+
+        ReminderEvent::create([
+            'reminder_id' => $reminder->id,
+            'site_id' => $site->id,
+            'date' => $measurement->date->addYear()->subWeek(),
+        ]);
+
+        ReminderEvent::create([
+            'reminder_id' => $reminder->id,
+            'site_id' => $site->id,
+            'date' => $measurement->date->addYear()->subMonth(),
+        ]);
 
         $measurement->plot->setLastMeasuredAt($measurement);
     }
