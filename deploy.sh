b5952ebd1b27dc85626f1e35bb6bc235a28d2ea6
noahcaldwell7 <noahgreycaldwell@gmail.com>
2022-02-10 13:51:15 -0600

    Production build

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git checkout master
+git pull
+npm run production \
+  && git add -A \
+  && git commit -m "Production build" \
+  && git push \
+  && git checkout live \
+  && git merge master \
+  && git push \
+  && git checkout master
