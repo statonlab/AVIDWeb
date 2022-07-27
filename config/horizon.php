4ecaf32f750371f8833d6f19139290994534145a
Abdullah Almsaeed <almasaeed2010@users.noreply.github.com>
2022-07-27 16:35:48 -0400

    Update config/horizon.php

--- config/horizon.php
+++ config/horizon.php
@@ -183,7 +183,7 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
+                'maxProcesses' => 1,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
