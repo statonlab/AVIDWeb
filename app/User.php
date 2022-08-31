459d91cdff4c49565bbdc30fa29aa39b61a84ab7
Abdullah Almsaeed <almasaeed2010@gmail.com>
2022-08-31 09:09:01 -0400

    Use soft deletes on users table

--- app/User.php
+++ app/User.php
@@ -4,13 +4,14 @@ namespace App;
 
 use App\ModelTraits\Permissions\HasRole;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable, HasRole, HasApiTokens;
+    use Notifiable, HasRole, HasApiTokens, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
