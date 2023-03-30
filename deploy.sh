b8d0a92416a3f9fc7e155b332488e080d0a8ae4d
Abdullah Almsaeed <almasaeed2010@gmail.com>
2023-03-30 14:51:26 -0400

    WIP -- merge

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,5 @@ npm run production \
   && git merge master \
   && git push \
   && git checkout master \
-  && curl https://forge.laravel.com/servers/369492/sites/1026800/deploy/http?token=1WDJChriPvKWxriAhhwg3OU1hFIs5w47UhKg2GG7 \
   && git push \
   && echo "Deployed successfully"
