46e6e03b9e97e19c69ee7e1e728de3186992fc45
Noah Caldwell <noahgreycaldwell@gmail.com>
2023-07-05 12:41:20 -0500

    last hotfix

--- resources/js/components/Reports/ReportByCounty.vue
+++ resources/js/components/Reports/ReportByCounty.vue
@@ -46,7 +46,7 @@
               {{ county.name }}
             </td>
             <td>
-              {{ county.state.name ?? '' }}
+              {{ county.state ? county.state.name : '' }}
             </td>
             <td>
               {{ county.sites_count }}
